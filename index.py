b6d5a1883786be337a82f70d91a1f9dad6864da4
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2022-10-15 20:40:38 -0600

    Add files via upload

--- index.py
+++ index.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template, request, jsonify, send_file
+from markupsafe import escape
 import pytube
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
-@app.route("/adios", methods=["GET", "POST"])
-def adios():
-    content = request.json
-    video = pytube.YouTube(content["url"])
+@app.route("/data_video", methods=["POST"])
+def data_video():
+    url = request.form["url"]
+    video = pytube.YouTube(url)
     imagen = video.thumbnail_url
     title = video.title
     lista = video.streams.first()
-    data = {
-        "imagen": imagen,
-        "title": title,
-    }
-    print(lista)
-    descarga = video.streams.first().download()
-    # descarga = video.streams.first()
-    # return jsonify(data) 
-    return send_file(descarga)
+    return render_template("index.html", title=title,  url=url, thumbail_url=imagen)
+
+@app.route("/video", methods=["POST"])
+def get_video():
+    url = request.form["url"]
+    return send_file(pytube.YouTube(url).streams.first().download(), as_attachment=True)
